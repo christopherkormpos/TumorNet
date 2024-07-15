6d3ed05baaceb9b22d7432ebb8044b01d1c295b0
unknown <christopher.kormpos@gmail.com>
2024-07-15 10:12:36 +0300

    Application.jsx complete comments

--- tumornet-webapp/src/components/Pages/Application.jsx
+++ tumornet-webapp/src/components/Pages/Application.jsx
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import '../../application.css'
-import PredictionChart from '../../components/PredictionChart'; 
+import PredictionChart from '../../components/PredictionChart';
 
 export function Application() {
-  const [base64Image, setBase64Image] = useState('');
-  const [prediction, setPrediction] = useState(null);
+  //UseStateHooks initialization
   const [selectedImage, setSelectedImage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [base64Image, setBase64Image] = useState('');
+  const [prediction, setPrediction] = useState(null);
   const [isPredicting, setIsPredicting] = useState(false);
 
+  // Handle image selection and conversion to base64
   const handleImageChange = () => {
     setIsLoading(true);
-    setPrediction(null);
+    setPrediction(null); // Reset prediction
     const reader = new FileReader();
     reader.onload = function (e) {
       const dataURL = reader.result;
       const base64 = dataURL.replace("data:image/png;base64,", "");
       setSelectedImage(dataURL); // Update selectedImage state with dataURL
-      setBase64Image(base64);
+      setBase64Image(base64); // Update base64Image state
       console.log(base64);
+      // Set loading state to false after 3 seconds then scroll to prediction button automatically
       setTimeout(() => {
         setIsLoading(false);
         const imageSelectedElement = document.getElementById('predict-button');
@@ -33,10 +36,10 @@ export function Application() {
     };
 
     if (document.getElementById('image-selector').files.length > 0) {
-      reader.readAsDataURL(document.getElementById('image-selector').files[0]);
+      reader.readAsDataURL(document.getElementById('image-selector').files[0]); // Read selected file as DataURL
     }
   };
-
+  // Add and remove event listener for image input
   useEffect(() => {
     document.getElementById('image-selector').addEventListener('change', handleImageChange);
 
@@ -44,56 +47,57 @@ export function Application() {
       document.getElementById('image-selector').removeEventListener('change', handleImageChange);
     };
   }, []);
-
+  // Handle prediction function
   const handlePrediction = () => {
     setIsPredicting(true); // Set predicting state to true
     let message = {
       image: base64Image
     };
 
-    console.log(message);
-
-    fetch("http://127.0.0.1:5000/predict", { 
+    console.log(message); //Prints the base64 image (debugging purposes only)
+    // Send data to server. Based on the port running the predict_app.py (Here is local host port 5000 /predict)
+    fetch("http://127.0.0.1:5000/predict", {
       method: 'POST',
       body: JSON.stringify(message),
     })
       .then(response => response.json())
       .then(data => {
         setTimeout(() => {
-          setPrediction(data.prediction);
-          console.log(data);
-          setIsPredicting(false);
+          setPrediction(data.prediction); // Update prediction state to true
+          console.log(data); //Print the prediction (number betwwen 0 and 1). Debugging purposes only
+          setIsPredicting(false); // Update prediction state to false
         }, 3000);
       })
       .catch(error => {
         console.error('Error:', error);
-        setIsPredicting(false);
+        setIsPredicting(false); //If an error occurs set predicting state to false
       });
   };
-
+  // Component to display prediction result based on the data.prediction. Can also be sepereted to the components folder 
+  //but because it is only used in the Application.jsx it is written inside here.
   const ValueDisplay = ({ value }) => {
     const isMalignant = value > 0.5;
     return (
       <div>
         {isMalignant ? (
           <div className="malignant">
-          <h2 className='malignant'>
-            The model predicts with&nbsp;<span className='conf'>high confidence</span>&nbsp;that the tumor in the provided image is&nbsp;
-            <span className='highlight-mal'>MALIGNANT.</span>
-          </h2>
-        </div>
+            <h2 className='malignant'>
+              The model predicts with&nbsp;<span className='conf'>high confidence</span>&nbsp;that the tumor in the provided image is&nbsp;
+              <span className='highlight-mal'>MALIGNANT.</span>
+            </h2>
+          </div>
         ) : (
           <div className="benign">
-        <h2 className='benign'>
-          The model predicts with&nbsp;<span className='conf'>high confidence</span>&nbsp;that the tumor in the provided image is&nbsp;
-          <span className='highlight-ben'>BENIGN.</span>
-        </h2>
-      </div>
+            <h2 className='benign'>
+              The model predicts with&nbsp;<span className='conf'>high confidence</span>&nbsp;that the tumor in the provided image is&nbsp;
+              <span className='highlight-ben'>BENIGN.</span>
+            </h2>
+          </div>
         )}
       </div>
     );
   };
-
+  //Render the Application UI
   return (
     <div>
       <h3>Please select an image file (supported types: <span className='h3highlight'>PNG</span>).</h3>
@@ -126,8 +130,8 @@ export function Application() {
         </div>
         {(!isPredicting && prediction) && (
           <>
-          <ValueDisplay value={prediction[0]} />
-          <PredictionChart prediction={prediction} />
+            <ValueDisplay value={prediction[0]} />
+            <PredictionChart prediction={prediction} />
           </>
         )}
       </div>
